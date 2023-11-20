cfefac7f05830102ed5635f0ad68f80419cd39a8
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-11-20 13:23:22 +0000

    modification syntax and add description to the struct

--- main.h
+++ main.h
@@ -7,12 +7,22 @@
 	/*PROTOTYPE FUNCTION and STRUCT : FILE "_printf.c"*/
 
 /*STRUCT*/
-typedef struct Format_type {
+
+/**
+ * struct Format_type - struct for char list and function call
+ *	adapted at the char
+ *
+ * @letter: char which indicated the format
+ * @pointer_function: function pointer to function adapted at the char "type"
+ */
+typedef struct Format_type
+{
 	char letter;
 	void (*pointer_function)(va_list current_arg);
 } Format_type;
 
 /*FUNCTION*/
+
 int _printf(const char *format, ...);
 
 	/*PROTOTYPE FUNCTION FILE : "format_print.c"*/
